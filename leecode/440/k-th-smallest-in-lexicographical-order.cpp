36bf142056cb513c33e662c07d6e09dcfc9690d2
neo zhao <654018509@qq.com>
2022-08-04 23:31:05 +0000

    [WIP] step max ones digit; result is correct, but time overflow

--- leecode/440/k-th-smallest-in-lexicographical-order.cpp
+++ leecode/440/k-th-smallest-in-lexicographical-order.cpp
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <string.h>
 #include <ctime>
+#include <algorithm>
+using namespace std;
 
 unsigned int string_to_int(const char *str);
 
@@ -25,6 +27,7 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
 // current_value should be unsigned long long. when current_value * 10, int value may overflow
 int mini_k_solution(unsigned long long current_value, unsigned int current_index, const unsigned int max_value, const unsigned int target_index)
 {
@@ -45,6 +48,21 @@ int mini_k_solution(unsigned long long current_value, unsigned int current_index
         }
         else
         {
+            int max_index_step = (target_index - current_index);
+            int last_value_step = 9 - (current_value % 10);
+            int max_value_step = (max_value - current_value);
+
+            int step = min(min(max_index_step, last_value_step), max_value_step);
+
+            current_value += step;
+            current_index += step;
+
+            if (current_index == target_index)
+            {
+                find = true;
+                break;
+            }
+
             // go upper
             while (9 == (current_value % 10) || current_value + 1 > max_value)
             {
